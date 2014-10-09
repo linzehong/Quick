17f5f28bf09ad89c6fefe0d1ae8646532e200164
linzehong <linzehong1211@126.com>
2014-10-09 14:23:36 +0800

    create project

---
+++ desktop/src/com/cvte/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cvte.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cvte.game.QuickGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QuickGame(), config);
+	}
+}
